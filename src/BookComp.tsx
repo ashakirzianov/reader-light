2a8789a345560735aae57efda68dc9245d58ada4
ashakirzianov <ashakirzianov@icloud.com>
2019-10-01 12:21:13 +0700

    Implement 'BookComp'

---
+++ src/BookComp.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import { Book, BookFragment } from 'booka-common';
+import { BookFragmentComp } from './reader';
+
+export type BookProps = {
+    book: Book,
+}
+
+export function BookComp(props: BookProps) {
+    const fragment: BookFragment = {
+        path: [],
+        nodes: props.book.volume.nodes,
+    };
+    return <BookFragmentComp
+        fragment={fragment}
+        fontFamily='Georgia'
+        fontSize={24}
+        color='black'
+        refColor='blue'
+        refHoverColor='purple'
+    />;
+}
