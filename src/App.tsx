30f1e4f45cacc702d00e2c40d6c136693945ac80
ashakirzianov <ashakirzianov@icloud.com>
2019-10-01 13:33:53 +0700

    Use @reach/router

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import { PublicBookComp } from './Public';
+import { Router } from '@reach/router';
 
+function BookRoute(props: any) {
+  return <PublicBookComp bookName={props.bookId} />;
+}
 const App: React.FC = () => {
-  return <PublicBookComp bookName='wap' />;
+  return <Router>
+    <BookRoute path='/book/:bookId' />
+  </Router>;
 }
 
 export default App;
