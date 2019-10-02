ae09e7bddc0a8048a624b5623dfc78ac0992e866
ashakirzianov <ashakirzianov@icloud.com>
2019-10-02 14:37:11 +0700

    Add ToC

--- src/BookComp.tsx
+++ src/BookComp.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { Book, fragmentForPath, BookPath } from 'booka-common';
 import { BookFragmentComp } from './reader';
+import { TableOfContents, tocForBook } from 'booka-common';
 
 export type BookProps = {
     book: Book,
@@ -13,10 +14,14 @@ export function BookComp({ book }: BookProps) {
         flexDirection: 'column',
         alignItems: 'center',
     }}>
+        <TableOfContentsComp
+            toc={tocForBook(book)}
+            navigateToPath={setPath}
+        />
         <PathLink
             path={fragment.previous}
             text='Previous'
-            onClick={setPath}
+            navigateToPath={setPath}
         />
         <div style={{
             maxWidth: '50em',
@@ -34,29 +39,62 @@ export function BookComp({ book }: BookProps) {
         <PathLink
             path={fragment.next}
             text='Next'
-            onClick={setPath}
+            navigateToPath={setPath}
         />
     </div>;
 }
 
 type PathLinkProps = {
     path?: BookPath,
-    onClick: (path: BookPath) => void,
+    navigateToPath: (path: BookPath) => void,
     text: string,
 };
-function PathLink({ path, onClick, text }: PathLinkProps) {
+function PathLink({ path, navigateToPath: onClick, text }: PathLinkProps) {
     return path === undefined
         ? null
-        : <button
-            onClick={() => {
+        : <a
+            href=''
+            onClick={e => {
+                e.preventDefault();
                 onClick(path);
                 window.scrollTo(0, 0);
             }}
             style={{
-                margin: '1em',
+                // margin: '1em',
                 fontSize: '2em',
             }}
         >
             {text}
+        </a>
+}
+
+type TableOfContentsProps = {
+    toc: TableOfContents,
+    navigateToPath: (path: BookPath) => void,
+}
+function TableOfContentsComp({ toc, navigateToPath }: TableOfContentsProps) {
+    const [visible, setVisible] = React.useState(false);
+
+    return <div style={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'flex-start',
+    }}>
+        <button onClick={() => setVisible(!visible)}>
+            Table of Contents
         </button>
+        {
+            !visible ? null :
+                toc.items.map(i =>
+                    <PathLink
+                        key={i.path.join('-')}
+                        path={i.path}
+                        text={i.title[0]}
+                        navigateToPath={p => {
+                            navigateToPath(p);
+                            setVisible(false);
+                        }}
+                    />)
+        }
+    </div>
 }
