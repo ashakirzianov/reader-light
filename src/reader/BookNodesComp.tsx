d86257c80434a4f051c01d6afccd711d5a31a2dc
ashakirzianov <ashakirzianov@icloud.com>
2019-10-03 23:46:58 +0700

    Fix 'applyAttrsRange' (again)

--- src/reader/BookNodesComp.tsx
+++ src/reader/BookNodesComp.tsx
@@ -303,7 +303,7 @@ function applyAttrsRange(fragments: RichTextFragment[], range: AttrsRange) {
     let start = 0;
     for (const frag of fragments) {
         const end = start + frag.text.length;
-        if (end <= range.start) {
+        if (end < range.start) {
             result.push(frag);
         } else if (start < range.start) {
             const pre: RichTextFragment = {
